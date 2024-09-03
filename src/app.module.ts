62ee12f3f31597d39dd91ab4473a76a21e6f658e
tavis161 <pawarate.eia@gmail.com>
2024-09-03 17:53:54 +0700

    Config DB and docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,5 @@
-// app.module.ts
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserModule } from './modules/user.module';
 import { TransactionModule } from './modules/transaction.module';
@@ -7,15 +7,32 @@ import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'database.db',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT, 10),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
       synchronize: true,
     }),
     UserModule,
     TransactionModule,
     AuthModule,
   ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
+
+
+
+
+
+
+
